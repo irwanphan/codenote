3039c8aa0d74f4a9cb7c645e23ee1e51db620df0
irwanphan <irwanphan@gmail.com>
2023-06-11 12:48:35 +0700

    limit default to 10 in scanned list, and have more

--- libs/components/CodeNoteSalesList/index.tsx
+++ libs/components/CodeNoteSalesList/index.tsx
@@ -1,30 +1,52 @@
 import { LoadingBlockList } from "@/libs/elements/LoadingBlock"
 import { useFetchSales } from "@/libs/hooks/sales/useFetchSales"
 import { Box, Text } from "@chakra-ui/react"
+import { useEffect, useState } from "react"
 
-const CodeNoteSalesList = () => {
+type CodeNoteSalesListProps = {
+    itemsShowed?: number
+}
+
+const CodeNoteSalesList = ({itemsShowed = 10}: CodeNoteSalesListProps) => {
     const { sales, isLoadingSales } = useFetchSales()
+    const [ haveMore, setHaveMore ] = useState<Boolean>(false)
+
+    // const haveMore = sales.length > itemsShowed
     if (isLoadingSales) return (<LoadingBlockList/>)
+
+    useEffect(() => {
+        if (!isLoadingSales) {
+            if (sales!.length > itemsShowed) {
+                setHaveMore(true)
+            }
+        }
+    }, [sales, isLoadingSales])
 
     return (
         <Box>
-            {sales?.map((sale:any, index:number) => {
+            {sales?.slice(0, 10).map((sale:any, index:number) => {
+                const createdAt = new Date(sale.createdAt).toLocaleString("id-ID", {
+                    month: 'long', day: 'numeric', year: 'numeric', 
+                    hour: 'numeric', minute: 'numeric', second: 'numeric'
+                })
+                console.log(createdAt)
+
                 return(
                     <Box key={index} p={3} mb={2} 
                         border='1px solid'
-                        borderColor='gray.200' 
+                        borderColor='gray.200'
                         borderRadius='md'>
-                        <Text fontSize='0.75rem'>
-                            {sale.createdAt}
+                        <Text fontSize='0.75rem' color='orange.400' fontWeight={600}>
+                            {createdAt}
                         </Text>
                             {sale.detail.map((detail:any, index:number) => {
                             return (
                                 <>
                                     <Text>
-                                        Product ID: {detail.productId}
+                                        ID Produk: {detail.productId}
                                     </Text>
                                     <Text>
-                                        Quantity: {detail.qty}
+                                        Jumlah: {detail.qty}
                                     </Text>
                                 </>
                             )
@@ -32,6 +54,11 @@ const CodeNoteSalesList = () => {
                     </Box>  
                 )
             })}
+            {haveMore && (
+                <Box>
+                    <Text>Have more</Text>
+                </Box>   
+            )}
         </Box>
     )
 }
