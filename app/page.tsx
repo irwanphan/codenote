02eddc86c47ff47f38eb708f5d72c07bd558c8ce
irwanphan <irwanphan@gmail.com>
2023-06-11 09:18:05 +0700

    fetch sales and show

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import './globals.css'
-import { Box, Divider } from '@chakra-ui/react'
+import { Box, Divider, Text } from '@chakra-ui/react'
 
 import { useAuth } from '@contexts/authContext'
 import TokoAuth from '@/libs/components/TokoAuth'
@@ -8,13 +8,14 @@ import FormSubmitButton from '@/libs/elements/FormSubmit'
 import { FiDivideSquare } from 'react-icons/fi'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import { useFetchSales } from '@/libs/hooks/sales/useFetchSales'
 
 export default function App() {
   const { session, isLoadingSession } = useAuth()
   const [ salesData, setSalesData ] = useState<any>(null)
   const [ isLoading, setIsLoading ] = useState<boolean>(true)
 
-  const { sales, isLoadingSales } = useSales()
+  const { sales, isLoadingSales } = useFetchSales()
 
   useEffect(() => {
     console.log(sales)
@@ -23,6 +24,39 @@ export default function App() {
   // if (isLoadingSession || isLoading) return (
   //   <>loading</>
   // )
+  const SalesList = () => {
+    if (isLoadingSales) return (
+      <Box>
+        loading sales
+      </Box>
+    )
+
+    return (
+      <Box>
+        {sales?.map((sale:any, index:number) => {
+          return(
+            <Box key={index} p={4} mb={4} bg='gray.100' borderRadius='md'>
+              <Text fontSize='0.75rem'>
+                {sale.createdAt}
+              </Text>
+              {sale.detail.map((detail:any, index:number) => {
+                return (
+                  <>
+                    <Text>
+                      Product ID: {detail.productId}
+                    </Text>
+                    <Text>
+                      Quantity: {detail.qty}
+                    </Text>
+                  </>
+                )
+              })}
+            </Box>  
+          )
+        })}
+      </Box>
+    )
+  }
 
   return (
     <main>
@@ -30,7 +64,7 @@ export default function App() {
 
           <Divider my={4} />
 
-            {/* {salesData?.map((sale:any, index:number) => {
+            {/* {!isLoadingSales && sales?.map((sale:any, index:number) => {
               return(
                 <Box key={index} p={4} mb={4} bg='gray.100' borderRadius='md'>
                   <Box as='h3' mb={2} fontWeight='bold'>
@@ -45,6 +79,7 @@ export default function App() {
                 </Box>  
               )
             })} */}
+            <SalesList/>
 
           <FormSubmitButton href="/sales" mr={2} px={3} 
             position='fixed' bottom={4} right={4}
