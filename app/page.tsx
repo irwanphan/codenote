8efaff4bc31ef506fc19d5b27d3a1dae2707ea6c
irwanphan <irwanphan@gmail.com>
2023-06-11 12:24:05 +0700

    set bottom margin on main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import './globals.css'
-import { Divider } from '@chakra-ui/react'
+import { Box, Divider } from '@chakra-ui/react'
 
 import { useAuth } from '@contexts/authContext'
 import TokoAuth from '@/libs/components/TokoAuth'
@@ -11,12 +11,12 @@ export default function App() {
   // const { session, isLoadingSession } = useAuth()
 
   return (
-    <main>
+    <Box p={3} mb={16}>
       <TokoAuth/>
       <Divider my={4} />
 
       <CodeNoteSalesList />
       <CodeNoteThumbButton />
-    </main>
+    </Box>
   ) 
 }
